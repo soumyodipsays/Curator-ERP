f7e20d4bff8f5653a20c50337b689dc2beff1603
soumyodipsays <soumyodiptheboss007@gmail.com>
2026-04-13 14:28:45 +0530

    ADD:Get ProductList in Json format

---
+++ Product/DAL/ProductListDAL.cs
@@ -0,0 +1,25 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using DataAccessLayer.Dapper;
+using Product.Models;
+
+
+namespace Product.DAL
+{
+    public class ProductListDAL
+    {
+        private readonly DapperConn _db = new DapperConn();
+
+        public List<ProductListModel> GetProductList(int CustomerID)
+        {
+            DynamicParameters param = new DynamicParameters();
+            param.Add("@CustomerID", CustomerID);
+            return _db.ExecuteMultipleRow<ProductListModel>("spProduct_GetList",param);
+        }
+
+    }
+}
